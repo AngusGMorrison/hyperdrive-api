9b018ad24f3829274826199ab42d9a5ca6146b0b
github username <github email address>
2019-11-18 17:55:19 +0000

    Added secret key env variable to get_token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::API
 
   private def get_token(payload)
-    JWT.issue_token(payload, )
-  end
-
+    JWT.issue_token(payload, ENV['JWT_SECRET_KEY'])
   end
 
 end

5ac97ccba37cd63139c8e666771fe7385501ed52
github username <github email address>
2019-11-22 10:33:35 +0000

    Sketched root folder find method

--- app/controllers/drive_controller.rb
+++ app/controllers/drive_controller.rb
@@ -1,7 +1,13 @@
 class DriveController < ApplicationController
 
   def show
-    
+    @current_user = get_current_user
+    respond_with_user_files
+  end
+
+  private def respond_with_user_files
+    folder = Folder.where(["user_id = ? AND name = ?", @current_user.id, "__root__"])
+    @files = @current_user.folder
   end
 
 end

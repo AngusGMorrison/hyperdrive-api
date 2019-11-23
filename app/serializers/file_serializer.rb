bbc0f9dbcda19469115a8d65c63d9525515f21f3
github username <github email address>
2019-11-23 15:19:57 +0000

    Created file serializer for show drive response

---
+++ app/serializers/file_serializer.rb
@@ -0,0 +1,29 @@
+class FileSerializer < Serializer
+  include Rails.application.routes.url_helpers
+
+  def initialize(files: [])
+    @files = files
+  end
+
+  def serialize
+    if @files.respond_to?(:map)
+      @files.map { |file| serialize_single_file(file) }
+    else
+      [ serialize_single_file(@files) ]
+    end
+  end
+
+  def serialize_single_file(file)
+    {
+      id: file.id,
+      filename: file.filename,
+      content_type: file.content_type,
+      extension: Folder.get_file_extension(file),
+      byte_size: file.byte_size,
+      download_url: rails_blob_url(file, disposition: 'attachment'),
+      created_at: file.created_at
+    }
+  end
+
+end
+
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -16,6 +16,17 @@ class UserSerializer < Serializer
     }
   end
 
+  def serialize_with_files_as_json(files)
+    serialize_with_files(files).to_json
+  end
+
+  def serialize_with_files(files)
+    serialized_user = { user: serialize }
+    file_serializer = FileSerializer.new(files: files)
+    serialized_user[:user][:files] = file_serializer.serialize()
+    serialized_user
+  end
+
   def serialize
     {
       id: @user.id,

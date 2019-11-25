7260c9fb0b0592cc83f344b1d82edc54b9281cc8
github username <github email address>
2019-11-25 18:17:37 +0000

    Create file route works with new document model

--- app/controllers/drive_controller.rb
+++ app/controllers/drive_controller.rb
@@ -15,8 +15,8 @@ class DriveController < ApplicationController
   end
 
   def create
-    @current_user = get_current_user
-    folder = @current_user.root_folder
+    current_user = get_current_user
+    folder = current_user.root_folder
     @document = Document.create(
       folder: folder,
       filename: params[:file].original_filename,
@@ -28,8 +28,7 @@ class DriveController < ApplicationController
   end
 
   private def respond_with_new_file
-    new_file = @folder.files.last
-    file_serializer = FileSerializer.new(files: new_file)
+    file_serializer = FileSerializer.new(files: @document.file_data)
     response_body = file_serializer.serialize_as_json()
     render json: response_body, status: 200
   end

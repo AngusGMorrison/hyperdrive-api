1104dc3ae303e055689f97bbb15f4f22fae62477
github username <github email address>
2019-11-26 15:39:03 +0000

    Drafted Document size validation

--- app/models/document.rb
+++ app/models/document.rb
@@ -4,6 +4,14 @@ class Document < ApplicationRecord
 
   has_one_attached :file_data, dependent: :purge
 
+  validate :document_size
+
+  private def document_size
+    if byte_size > user.remaining_storage
+      errors.add(:byte_size, "is greater than remaining storage")
+    end
+  end
+
   def get_file_extension
     self.filename.to_s.match(/\..+\z/)[0]
   end

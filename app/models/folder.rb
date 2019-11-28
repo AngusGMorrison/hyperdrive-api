a02b30699635a6724f2b5c6f39809493280935eb
Angus Morrison <contact@angus-morrison.com>
2019-11-28 16:59:37 +0000

    Folder-subfolder relationship working correctly

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,7 +1,7 @@
 class Folder < ApplicationRecord
 
-  belongs_to :user
-  has_many :subfolders, class_name: :Folder, foreign_key: :containing_folder
+  belongs_to :containing_folder, polymorphic: true
+  has_many :subfolders, class_name: :Folder, as: :containing_folder
   has_many :documents, as: :containing_folder
 
 end

a4a18b943408f76617c4bb360d8a1069d739a7dd
Angus Morrison <contact@angus-morrison.com>
2019-11-29 10:46:30 +0000

    Added outline for folder serializer

---
+++ app/serializers/folder_serializer.rb
@@ -0,0 +1,35 @@
+class FolderSerializer
+
+  def initialize(folder:)
+    @folder = folder
+  end
+
+  def serialize
+    {
+      id: @folder.id,
+      name: @folder.name,
+      containing_folder_id: @folder.containing_folder_id,
+      documents: get_serialized_documents,
+      subfolders: serialize_subfolders,
+      created_at: @folder.created_at,
+      updated_at: @folder.updated_at
+    }
+  end
+
+  private def get_serialized_documents
+    doc_serializer = DocumentSerializer.new(documents: @folder.documents)
+    doc_serializer.serialize()
+  end
+
+  private def serialize_subfolders
+    @folder.subfolders.map do |subfolder|
+      {
+        folder_id: subfolder.id,
+        name: subfolder.name,
+        created_at: subfolder.created_at,
+        updated_at: subfolder.updated_at
+      }
+    end
+  end
+
+end

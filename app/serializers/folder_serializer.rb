4b4fe483edf7b7227bb129af23a5c8348d10b516
Angus Morrison <contact@angus-morrison.com>
2019-12-02 09:58:15 +0000

    Refactored parent and subfolder serialization to use shared serialize_relative method

--- app/serializers/folder_serializer.rb
+++ app/serializers/folder_serializer.rb
@@ -9,40 +9,34 @@ class FolderSerializer
       id: @folder.id,
       name: @folder.name,
       parent_folders: @folder.parent_folder ? serialize_parent_folders(@folder.parent_folder, []) : [],
-      documents: get_serialized_documents,
       subfolders: serialize_subfolders,
+      documents: get_serialized_documents,
       created_at: DateFormatter.format_date(@folder.created_at),
       updated_at: DateFormatter.format_date(@folder.updated_at)
+    }
+  end
+
+  private def serialize_parent_folders(parent, array)
+    array << serialize_relative(parent)
+    parent.parent_folder ? serialize_parent_folders(parent.parent_folder, array) : array
+  end
+
+  private def serialize_subfolders
+    @folder.subfolders.map { |subfolder| serialize_relative(subfolder) }
+  end
+
+  private def serialize_relative(relative)
+    {
+      id: relative.id,
+      name: relative.name,
+      created_at: relative.created_at,
+      updated_at: relative.updated_at
     }
   end
 
   private def get_serialized_documents
     doc_serializer = DocumentSerializer.new(documents: @folder.documents)
     doc_serializer.serialize()
-  end
-
-  private def serialize_parent_folders(parent, array)
-    array.push({
-      id: parent.id,
-      name: parent.name,
-      created_at: parent.created_at,
-      updated_at: parent.updated_at
-    })
-    if parent.parent_folder
-      serialize_parent_folders(parent.parent_folder, array)
-    end
-    array
-  end
-
-  private def serialize_subfolders
-    @folder.subfolders.map do |subfolder|
-      {
-        folder_id: subfolder.id,
-        name: subfolder.name,
-        created_at: subfolder.created_at,
-        updated_at: subfolder.updated_at
-      }
-    end
   end
 
 end

7c8e15e16cb1d9d19ff26e7cabbe2016a729f1be
Angus Morrison <contact@angus-morrison.com>
2019-12-12 17:43:56 +0000

    Rubocopped Document

--- app/models/concerns/validation.rb
+++ app/models/concerns/validation.rb
@@ -11,10 +11,14 @@ module Validation
     PASSWORD = "needs min. 8 chars: 1 number, 1 upper, 1 lower, 1 special."
 
     FOLDER = {
-      immutable_root: "Root folder cannot be modified",
-      parent_folder_required: "is required for non-root folders",
-      root_has_parent: "cannot be added to a root folder",
-      own_parent: "must be different to current folder"
+      immutable_root: "Root folder cannot be modified.",
+      parent_folder_required: "is required for non-root folders.",
+      root_has_parent: "cannot be added to a root folder.",
+      own_parent: "must be different to current folder."
+    }
+
+    DOCUMENT = {
+      byte_size: "is greater than remaining storage."
     }
   end
 
